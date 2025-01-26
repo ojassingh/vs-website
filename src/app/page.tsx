99b36b6a2c0017888af8faf32d3b4d141bcbd74b
Ojas Singh <ojas.singh02@gmail.com>
2025-01-25 22:17:21 -0500

    Fix media breakpoint issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-6xl overflow-x-hidden">
+    <main className="mx-auto max-w-6xl overflow-x-hidden sm:overflow-x-visible">
       <div
-        className="fixed inset-0 z-0 opacity-5 h-screen w-screen"
+        className="fixed inset-0 z-0 opacity-5"
         style={{
           backgroundImage: `url(${grain.src})`,
         }}
