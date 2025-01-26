e9e5c31113d16d5b154663de81b4e1ea623b4787
Ojas Singh <ojas.singh02@gmail.com>
2025-01-26 16:24:49 -0500

    Fix: Lint error causing type issue

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Dot } from "lucide-react";
 
 // const metadata: Metadata = {
