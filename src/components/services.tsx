ba6b5fc2f6da2a18d6419a7554811f328d9832ac
Ojas Singh <ojas.singh02@gmail.com>
2025-02-01 04:56:01 -0500

    Add new blog

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,9 +1,12 @@
 import {
+  Briefcase,
   Eye,
   Handshake,
   Landmark,
   Lightbulb,
   Speech,
+  TrendingUp,
+  Users,
   Wind,
 } from "lucide-react";
 import { FC } from "react";
@@ -65,6 +68,33 @@ const Services: FC = () => {
       ],
       icon: <Eye size={20} />,
     },
+    {
+      title: "Strategic Manpower Planning & Hiring",
+      description: [
+        "Align workforce strategies with business goals",
+        "Onboard talent that drives innovation",
+        "Build agile teams to adapt to market demands",
+      ],
+      icon: <Users size={20}/>,
+    }, 
+    {
+      title: "Talent Acquisition & Management",
+      description: [
+        "Source high-impact candidates for critical roles",
+        "Create structured career paths for team growth",
+        "Align employee goals with organizational vision",
+      ],
+      icon: <Briefcase size={20} />,
+    },
+    {
+      title: "Training Analysis & Team Engagement",
+      description: [
+        "Identify skill gaps to deliver targeted learning",
+        "Foster a culture of continuous improvement",
+        "Enhance workplace performance and morale",
+      ],
+      icon: <TrendingUp size={20} />,
+    },
   ];
 
   return (
@@ -73,11 +103,11 @@ const Services: FC = () => {
         <p className="max-w-max rounded-lg border px-2 py-1 text-sm">
           SERVICES
         </p>
-        <div className="sm:flex items-start justify-between">
-          <h1 className={`font-heading text-3xl sm:text-5xl font-medium`}>
+        <div className="items-start justify-between sm:flex">
+          <h1 className={`font-heading text-3xl font-medium sm:text-5xl`}>
             Leading teams with legal <br /> expertise globally
           </h1>
-          <p className="max-w-lg text-left mt-2 sm:mt-0 sm:text-right text-gray-600">
+          <p className="mt-2 max-w-lg text-left text-gray-600 sm:mt-0 sm:text-right">
             We guide founders and corporate leaders through the complexities of
             mergers, governance, compliance, and disputes—crafting strategies
             that align with their vision and scale with their ambitions.
@@ -85,11 +115,11 @@ const Services: FC = () => {
         </div>
       </div>
 
-      <div className="sm:mt-20 mt-8 grid grid-cols-1 gap-4 sm:grid-cols-3">
+      <div className="mt-8 grid grid-cols-1 gap-4 sm:mt-20 sm:grid-cols-3">
         {services.map(({ title, description, icon }, index) => {
           return (
             <BlurFade key={index} inView delay={index / 10}>
-              <div className="z-10 flex flex-col gap-4 rounded-md border bg-white/40 sm:px-8 sm:py-8 px-8 py-4 ring-1 shadow-md ring-black/5 backdrop-blur-md">
+              <div className="z-10 flex flex-col gap-4 rounded-md border bg-white/40 px-8 py-4 ring-1 shadow-md ring-black/5 backdrop-blur-md sm:px-8 sm:py-8">
                 <h3 className="flex items-center gap-2">
                   <span className="">{icon}</span>
                   {title}
